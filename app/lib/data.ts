ec41ca8246a52488eb0cff66c5c8ca3b98ef3afd
GabrielNathanael <gabriel@student.undiksha.ac.id>
2025-12-16 15:21:59 +0800

    step 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,4 @@
+// app\lib\data.ts
 import postgres from "postgres";
 import {
   CustomerField,
@@ -159,7 +160,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
